fdba6355c855594eab8687a4b371ccf0e038f5d3
yangpai <794898086@qq.com>
2022-04-09 11:54:51 +0800

    去掉Auth中的Feign调用

--- mall-auth/src/main/java/com/yangpai/auth/AuthApplication.java
+++ mall-auth/src/main/java/com/yangpai/auth/AuthApplication.java
@@ -11,7 +11,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @SpringBootApplication
 @EnableResourceServer
-@EnableFeignClients
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);}
